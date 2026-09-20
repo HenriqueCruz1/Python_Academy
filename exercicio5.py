1e8dbf07e992a9b4c9c65d493a7d34aa20198045
HenriqueCruz1 <henrique.almeida96@outlook.com>
2026-09-20 16:23:07 -0300

    calculo de aréa

--- exercicio5.py
+++ exercicio5.py
@@ -9,3 +9,8 @@ Saída
 Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas,
  não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error"."""
 
+valor_raio = int(input("Digite o valor do raio: "))
+pi = 3.14159
+area = pi * (valor_raio**2)
+
+print(f"Raio = {valor_raio}, Area = {area:.2f}")
